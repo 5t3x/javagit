7299a069f48e28fcd6ee8e537b30f33dd9e54b31
5t3x <kissmatemail@gmail.com>
2024-10-24 15:42:32 +0200

    letrejott: NetBeans projekt

---
+++ src/javagit/JavaGit.java
@@ -0,0 +1,16 @@
+package javagit;
+
+/**
+ *
+ * @author KissMáté(Szf_N_2024_
+ */
+public class JavaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

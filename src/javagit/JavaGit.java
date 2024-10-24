addec526a4c03f0beddb4f68e2fed30d8bd214b7
5t3x <kissmatemail@gmail.com>
2024-10-24 15:46:23 +0200

    commitok kitorolve

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -1,16 +1,9 @@
 package javagit;
 
-/**
- *
- * @author KissMáté(Szf_N_2024_
- */
 public class JavaGit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }

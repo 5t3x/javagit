de95d351d8629b59422c69e3f4ce480fa3b0ea90
5t3x <kissmatemail@gmail.com>
2024-10-24 15:48:54 +0200

    komment: feladat leirasa

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -3,7 +3,8 @@ package javagit;
 public class JavaGit {
 
     public static void main(String[] args) {
-        
+        /* ha hideg van viszek kabatot
+        egyebkent nem viszek kabatot */
     }
     
 }
